f74aa387cd16a3349222895134703d06dcfd0241
tanasecucliciu <tanasecucliciu@gmail.com>
2014-05-15 15:19:19 +0300

    Mic change. Eu nu fac modulul 4 il fac pe 3. Am gresit text-ul de la buton. Voi reveni cu update-uri semnificante mai pe seata

--- ProiectPAWInterfataSGBDSolution/ProiectPAWInterfataSGBD/FormularPrincipal.Designer.cs
+++ ProiectPAWInterfataSGBDSolution/ProiectPAWInterfataSGBD/FormularPrincipal.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Modulul4: SQL Editor";
+            this.button1.Text = "Modulul 3: SQL Editor";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
