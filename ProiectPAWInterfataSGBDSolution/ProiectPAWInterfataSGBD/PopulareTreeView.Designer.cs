3fbbb7076583e2036fd4a0e9d4a89c37b413d7d9
tanasecucliciu <tanasecucliciu@gmail.com>
2014-05-21 02:06:20 +0300

    ++1

--- ProiectPAWInterfataSGBDSolution/ProiectPAWInterfataSGBD/PopulareTreeView.Designer.cs
+++ ProiectPAWInterfataSGBDSolution/ProiectPAWInterfataSGBD/PopulareTreeView.Designer.cs
@@ -28,19 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(229, 289);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Populare";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // treeView1
             // 
@@ -56,7 +45,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(417, 382);
             this.Controls.Add(this.treeView1);
-            this.Controls.Add(this.button1);
             this.Name = "PopulareTreeView";
             this.Text = "PopulareTreeView";
             this.Load += new System.EventHandler(this.PopulareTreeView_Load);
@@ -67,7 +55,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TreeView treeView1;
     }
 }
--- ProiectPAWInterfataSGBDSolution/ProiectPAWInterfataSGBD/PopulareTreeView.cs
+++ ProiectPAWInterfataSGBDSolution/ProiectPAWInterfataSGBD/PopulareTreeView.cs
@@ -14,11 +14,11 @@ namespace ProiectPAWInterfataSGBD
 {
     public partial class PopulareTreeView : Form
     {
-        public static string connString = "user id=dima.david1;" +
-                                   "password=12345;server=localhost;" +
-                                   "database=david; " +
-                                   "connection timeout=10";
-        MySqlConnection cn = new MySqlConnection(connString);
+        //public static string connString = "user id=dima.david1;" +
+        //                           "password=12345;server=localhost;" +
+        //                           "database=david; " +
+        //                           "connection timeout=10";
+        //MySqlConnection cn = new MySqlConnection(connString);
         public static List<List<string>> colList = new List<List<string>>();
         public static List<string> tabList = new List<string>();
         public static string numeBD;
@@ -63,49 +63,49 @@ namespace ProiectPAWInterfataSGBD
 
         private void PopulareTreeView_Shown(object sender, EventArgs e)
         {
-            try
-            {
-                cn.Open();
-            }
-            catch (MySqlException ex)
-            {
-                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Application.ExitThread();
-            }
+            //try
+            //{
+            //    cn.Open();
+            //}
+            //catch (MySqlException ex)
+            //{
+            //    MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //    Application.ExitThread();
+            //}
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            treeView1.Nodes.Clear();
+            //treeView1.Nodes.Clear();
 
-            MySqlCommand cm = new MySqlCommand("SELECT * from Angajati",cn);
+            //MySqlCommand cm = new MySqlCommand("SELECT * from Angajati",cn);
 
-            try
-            {
+            //try
+            //{
                
-               MySqlDataReader datar = cm.ExecuteReader();
+            //   MySqlDataReader datar = cm.ExecuteReader();
 
-               while (datar.Read())
-               {
-                   TreeNode nod = new TreeNode(datar["id"].ToString());
-                   nod.Nodes.Add(datar["nume"].ToString());
-                   TreeNode Nod2 = new TreeNode();
-                   nod.Nodes.Add(Nod2);
-                   Nod2.Text = "Mata";
-                   Nod2.Nodes.Add(new TreeNode("Tactu"));
+            //   while (datar.Read())
+            //   {
+            //       TreeNode nod = new TreeNode(datar["id"].ToString());
+            //       nod.Nodes.Add(datar["nume"].ToString());
+            //       TreeNode Nod2 = new TreeNode();
+            //       nod.Nodes.Add(Nod2);
+            //       Nod2.Text = "Mata";
+            //       Nod2.Nodes.Add(new TreeNode("Tactu"));
                    
 
-                   //nod.Nodes.Add(datar["prenume"].ToString());
-                   //nod.Nodes.Add(datar["varsta"].ToString());
-                   nod.Nodes.Add(datar["Salariul"].ToString());
-                   treeView1.Nodes.Add(nod);
+            //       //nod.Nodes.Add(datar["prenume"].ToString());
+            //       //nod.Nodes.Add(datar["varsta"].ToString());
+            //       nod.Nodes.Add(datar["Salariul"].ToString());
+            //       treeView1.Nodes.Add(nod);
 
-               }
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
+            //   }
+            //}
+            //catch(Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
 
             }
 
@@ -116,14 +116,14 @@ namespace ProiectPAWInterfataSGBD
 
         private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
         {
-            Console.WriteLine("click!");
+            //Console.WriteLine("click!");
             
-            TreeNode ChildNode1 = new TreeNode();
-            ChildNode1.Text = "Next Child 1";
-            TreeNode ChildNode2 = new TreeNode();
-            ChildNode2.Text = "Next Child 2";
-            ChildNode1.Nodes.Add(ChildNode2);
-            treeView1.SelectedNode.Nodes.Add(ChildNode1);
+            //TreeNode ChildNode1 = new TreeNode();
+            //ChildNode1.Text = "Next Child 1";
+            //TreeNode ChildNode2 = new TreeNode();
+            //ChildNode2.Text = "Next Child 2";
+            //ChildNode1.Nodes.Add(ChildNode2);
+            //treeView1.SelectedNode.Nodes.Add(ChildNode1);
         }
 
         }
